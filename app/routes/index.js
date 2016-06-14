b3152b43980068a0c4a6720ae5140f86791c6bc3
Daniel Wulff and Peter Beach <daniel@>
2016-06-14 09:42:57 -0700

    zip search, committees and subcommittees working

---
+++ app/routes/index.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  actions: {
+    zipSearch(params) {
+      this.transitionTo('results', params.zip);
+    },
+    committeeSearch() {
+      this.transitionTo('resultscommittees');
+    }
+  }
+});
